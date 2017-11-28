f4af245cdf9dbea6af1e7e634a2978f18411ab29
PhantomGitHub <stav1472@gmail.com>
2017-11-28 20:48:01 +0200

    code conventions

--- Database/AddToDB.py
+++ Database/AddToDB.py
@@ -39,9 +39,10 @@ def get_access_permission_of_employee_by_id(id):
     employee = Employees.find_one({"id": int(id)})
     return int(employee["permission"])
 
-#this function gets date time in format "D/M/Y Hour", the room from the DB to assign employees
-#and number of employees to assign.
-#output: False - in case the employees can not be assigned to the room
+
+# this function gets date time in format "D/M/Y Hour", the room from the DB to assign employees
+# and number of employees to assign.
+# output: False - in case the employees can not be assigned to the room
 # True - in case the employees were assigned to the room
 def assign_employees_to_room(date_time, room, num_employees):
     global Rooms
@@ -56,5 +57,5 @@ def assign_employees_to_room(date_time, room, num_employees):
         if schedule[converted_date_time][0] + num_employees > capacity:
             return False
         schedule[converted_date_time] = (schedule[converted_date_time][0] + num_employees, None)
-    Rooms.replace_one({'_id':room['_id']},room)
+    Rooms.replace_one({'_id': room['_id']}, room)
     return True
