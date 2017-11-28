c756cc7c3949fbe8bbc85991b7b19c1a89d5cf03
PhantomGitHub <stav1472@gmail.com>
2017-11-28 23:18:46 +0200

    corrected test files with newline
    tests now passing

--- App/Room.py
+++ App/Room.py
@@ -1,14 +1,14 @@
 class Room:
     # list of sched_item
 
-    def __init__(self, id, floor, max_capacity, schedule_item):
+    def __init__(self, id, floor, max_capacity):
         self.id = id
         self.floor = floor
         self.maxCapacity = max_capacity
-        self.schedule_item = schedule_item
+        self.schedule = {}
 
     def print_floor(self):
         print "The room's floor is: ", self.floor
 
-    def size_list_schedule_item(self):
-        print len(self.schedule_item)
+    def size_list_schedule(self):
+        print len(self.schedule)
