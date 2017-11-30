f2b62c744523170923eff49e14becdd9467077b8
Koby Brandes <Koby.Brandes10@gmail.com>
2017-11-30 14:55:24 +0200

    bug fix

--- App/RoomReccomendations.py
+++ App/RoomReccomendations.py
@@ -3,11 +3,11 @@ import subprocess
 import os
 
 
-def ReccomendationToEmployeeByRoom(date_time, occupancy):
+def reccomendationToEmployeeByRoom(date_time, occupancy):
     reccomendedList=[]
-    for i in xrange(10):
+    for i in xrange(db.collection.count()):
         room = Rooms.find()[i]
-        if occupancy<=room.schedule[date_time].occupancy :
+        if occupancy<=room.maxCapacity-room.schedule[date_time].occupancy:
             reccomendedList.append(room)
     return reccomendedList
 
