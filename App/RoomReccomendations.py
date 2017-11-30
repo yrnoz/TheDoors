92089ad5f16fecd692bcd35953d57667cd5e06af
Koby Brandes <Koby.Brandes10@gmail.com>
2017-11-30 15:07:55 +0200

    bug fix and additions

--- App/RoomReccomendations.py
+++ App/RoomReccomendations.py
@@ -11,3 +11,15 @@ def reccomendationToEmployeeByRoom(date_time, occupancy):
             reccomendedList.append(room)
     return reccomendedList
 
+
+# input: time requested to check num of empty places in each room.
+# output: a dictionary that for each room include the number of empty seats in that room in the given time.
+def emptyRooms(time):
+    emptyPlaceInRooms = {}
+    for i in xrange(db.collection.count()):
+        room = Rooms.find()[i]
+        emptyPlaceInRooms.update(room.id, room.maxCapacity-room.schedule[time].occupancy)
+    return emptyPlaceInRooms
+
+
+
