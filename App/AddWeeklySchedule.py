bbb1c3d4398c6020195d9fbc7749531ede8c92df
aviadRozenknof <rozen501@gmail.com>
2017-12-03 14:59:03 +0200

    add weekly schedule, input: csv file #13

---
+++ App/AddWeeklySchedule.py
@@ -0,0 +1,25 @@
+from Database.ManageDB import *
+
+def weekly_schedule_for_employee(id, inputfile):
+    if check_id_of_employee(id) == False:
+        return "Employee doesn't exist in the system"
+    employee = find_employee(id)
+    employee_permission = get_access_permission_of_employee_by_id(id)
+    rooms_assigned = ""
+    with open(inputfile) as schedule:
+        for line in schedule.readlines():
+            if line.count(',') == 2:
+                date, room_permission, num_employees = line.split(',')
+            else:
+                date, room_permission = line.split(',')
+                num_employees = 1
+            if room_permission < employee_permission:
+                return "You don't have the right access permission"
+            matching_rooms = Rooms.find({"permission" : int(room_permission)})
+            if matching_rooms.count() == 0:
+                return "There is no room matching to the permission - " + str(room_permission)
+            for room in matching_rooms:
+                if (assign_employees_to_room_one_hour(date, room, num_employees)):
+                    rooms_assigned += " " + room["id"]
+                    break
+    return rooms_assigned
