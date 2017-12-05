7acfd5fce3460b82e5ec88ed7ff7ed5113404670
Yarin <yarin129@gmail.com>
2017-12-05 20:33:15 +0200

    added tests for importing rooms and employees details

--- Tests/DB_test.py
+++ Tests/DB_test.py
@@ -15,26 +15,41 @@ from Database.ManageDB import *
 #
 #     p.terminate()
 
-#@pytest.mark.skip(reason="should be separated into different tests")
+def get_ids_from_file(file_name):
+    list_ids = []
+    with open(file_name) as details:
+        for line in details.readlines():
+            list_ids.append(line.split(',')[0])
+    return list_ids
+
+
 def test_import_employees_succeeds():
+    file_name = "Resources%semployees_test.csv" % os.sep
+    employee_ids = get_ids_from_file(file_name)
     p = subprocess.Popen('mongod', stdout=open(os.devnull, "w"))
     Employees.drop()
-    import_employees_from_file("Resources%semployees_test.csv" % os.sep)
-    assert Employees.count() == 10
-
+    import_employees_from_file(file_name)
+    assert Employees.count() == 11
+    for id in employee_ids:
+        assert Employees.find_one({"id": id}) is not None
+    Employees.drop()
     p.terminate()
 
 
-#@pytest.mark.skip(reason="should be separated into different tests")
 def test_import_rooms_succeeds():
+    file_name = "Resources%srooms_test.csv" % os.sep
+    room_ids = get_ids_from_file(file_name)
     p = subprocess.Popen('mongod', stdout=open(os.devnull, "w"))
     Rooms.drop()
-    import_room_details_from_file("Resources%srooms_test.csv" % os.sep)
+    import_room_details_from_file(file_name)
     assert Rooms.count() == 11
+    for id in room_ids:
+        assert Rooms.find_one({"id": id}) is not None
+    Rooms.drop()
     p.terminate()
 
 
-#@pytest.mark.skip(reason="should be separated into different tests")
+# @pytest.mark.skip(reason="should be separated into different tests")
 def test_db():
     p = subprocess.Popen('mongod', stdout=open(os.devnull, "w"))
     Rooms.drop()
@@ -45,8 +60,8 @@ def test_db():
     # employee = Employee(777, "John", "Engineer", 2)
     # add_employee(employee)
     assert check_id_of_employee("123") is True
-    employee = find_employee(777)
-    print (employee)
+    #employee = find_employee(777)
+    #print (employee)
     # assign_employees_to_room_one_hour('24/07/17 12', room, 10)
     # assign_employees_to_room_to_X_hours('24/07/17 12', 10, 3)
     # assign_employees_to_room_to_X_hours('24/07/17 12', 70, 10)
