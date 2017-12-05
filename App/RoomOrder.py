8fb5e709dc6c625bacc84e9edee8782ab7bbb85a
Yarin <yarin129@gmail.com>
2017-12-05 19:48:47 +0200

    some changes

--- App/RoomOrder.py
+++ App/RoomOrder.py
@@ -1,5 +1,5 @@
 class RoomOrder:
-	def __init__(self, date_time, num_hours, num_employees):
-		self.date_time = date_time
-		self.num_hours = num_hours
-		self.num_employees = num_employees
+    def __init__(self, date_time, num_hours, num_employees):
+        self.date_time = date_time
+        self.num_hours = num_hours
+        self.num_employees = num_employees
--- Tests/logic_test.py
+++ Tests/logic_test.py
@@ -1,6 +1,6 @@
 def test_logic():
-	best_rooms = {1: 4, 3: 5, 2: 4}
-	sorted_rooms = sorted(best_rooms.items(), key=lambda y: y[1])
-	print(sorted_rooms[::-1])
-	for x in sorted_rooms:
-		print(x[1])
+    best_rooms = {1: 4, 3: 5, 2: 4}
+    sorted_rooms = sorted(best_rooms.items(), key=lambda y: y[1])
+    print(sorted_rooms[::-1])
+    for x in sorted_rooms:
+        print(x[1])
