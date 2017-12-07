27137fa3bb8fa448550de8b5e09bf843195c9cd3
Koby Brandes <Koby.Brandes10@gmail.com>
2017-12-07 13:13:50 +0200

    add weekly schedule test

--- App/RoomReccomendations.py
+++ App/RoomReccomendations.py
@@ -1,44 +1,41 @@
-from Database.ManageDB import Rooms
+class Recommendation:
+	def __init__(self, room_table, employee_table):
+		self.Rooms = room_table
+		self.Employees = employee_table
 
+	def recommend_by_friends(self, employee):
+		"""return list of rooms 'res' that highly recommended for employee """
+		rec_room = employee.recommendation_by_friends()
+		res = []
+		for room in rec_room:
+			room_tmp = self.Rooms.find({"id": room[0]})
+			if room.current_occupancy <= room_tmp.capcity:
+				res.append(room[0])
+		return res
 
-def recommend_by_friends(employee):
-    """return list of rooms 'res' that highly recommended for employee """
-    rec_room = employee.recommendation_by_friends()
-    res = []
-    for room in rec_room:
-        room_tmp = Rooms.find({"id": room[0]})
-        if room.current_occupancy <= room_tmp.capcity:
-            res.append(room[0])
-    return res
+	def reccomendationToEmployeeByRoom(self, date_time, occupancy):
+		reccomendedList = []
+		for room in self.Rooms.find():
+			if occupancy <= room.maxCapacity - room.schedule[date_time].occupancy:
+				reccomendedList.append(room)
+		return reccomendedList
 
+	# input: time requested to check num of empty places in each room.
+	# output: a dictionary that for each room include the number of empty seats in that room in the given time.
+	def emptyRooms(self, time):
+		emptyPlaceInRooms = {}
+		for room in self.Rooms.find():
+			emptyPlaceInRooms.update(room.id, room.maxCapacity - room.schedule[time].occupancy)
+		return emptyPlaceInRooms
 
-def reccomendationToEmployeeByRoom(date_time, occupancy):
-    reccomendedList = []
-    for room in Rooms:
-        if occupancy <= room.maxCapacity - room.schedule[date_time].occupancy:
-            reccomendedList.append(room)
-    return reccomendedList
-
-
-# input: time requested to check num of empty places in each room.
-# output: a dictionary that for each room include the number of empty seats in that room in the given time.
-def emptyRooms(time):
-    emptyPlaceInRooms = {}
-    for room in Rooms.find():
-        emptyPlaceInRooms.update(room.id, room.maxCapacity - room.schedule[time].occupancy)
-    return emptyPlaceInRooms
-
-
-def room_with_my_friends(friends):
-    """this fun get list of friends which the user want to be with (in the same room) and return the room that
-    the biggest sub group in it"""
-    rooms = {}
-    for friend in friends:
-        location = friend.get_location()
-        if location is not None:
-            count = rooms.get(location, default=0) + 1
-            rooms += (location, count)
-    sorted_rooms = sorted(rooms, key=lambda x: x[1])
-    return sorted_rooms[-1]
-
-
+	def room_with_my_friends(self, friends):
+		"""this fun get list of friends which the user want to be with (in the same room) and return the room that
+		the biggest sub group in it"""
+		rooms = {}
+		for friend in friends:
+			location = friend.get_location()
+			if location is not None:
+				count = rooms.get(location, default=0) + 1
+				rooms += (location, count)
+		sorted_rooms = sorted(rooms, key=lambda x: x[1])
+		return sorted_rooms[-1]
--- Tests/logic_test.py
+++ Tests/logic_test.py
@@ -1,6 +1,32 @@
-def test_logic():
-    best_rooms = {1: 4, 3: 5, 2: 4}
-    sorted_rooms = sorted(best_rooms.items(), key=lambda y: y[1])
-    print(sorted_rooms[::-1])
-    for x in sorted_rooms:
-        print(x[1])
+import os
+import subprocess
+import sys
+import pytest
+
+from App.AddWeeklySchedule import add_weekly_schedule_for_employee
+
+sys.path.append(os.getcwd())
+from App.Employee import Employee
+from App.RoomOrder import RoomOrder
+from Database.ManageDB import *
+
+
+def test_add_weekly_schedule():
+    p = subprocess.Popen('mongod', stdout=open(os.devnull, "w"))
+    Rooms.drop()
+    Employees.drop()
+    employees = open("employees.csv", "w+")
+    rooms = open("rooms.csv", "w+")
+    # entering two employees with permissions 2.
+    employees.write("234,Koby,Engineer,2")
+    employees.write("498,Elyasaf,Engineer,2")
+    # entering a room with permission 1.
+    rooms.write("taub 4,40,1,1")
+    import_employees_from_file(employees.name)
+    import_room_details_from_file(rooms.name)
+    schedule_file = open("schedule_file.csv", "w+")
+    schedule_file.write("'24/07/17 12', 3, 170")
+    # checking the validity of the id of the employee.
+    assert add_weekly_schedule_for_employee("000", schedule_file) is "Employee doesn't exist in the system"
+    # checking the permissions of the employee and the permissions of the room.
+    assert add_weekly_schedule_for_employee("789", schedule_file) is "You don't have the right access permission"
