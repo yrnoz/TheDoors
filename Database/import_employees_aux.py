be9ed0533f8d07cd91db86eca8a9af7400e68485
Yarin <yarin129@gmail.com>
2018-01-13 21:44:08 +0200

    how sending jqueries should look like (#83)

---
+++ Database/import_employees_aux.py
@@ -0,0 +1,4 @@
+import sys
+from ManageDB import *
+if __name__ == '__main__':
+    import_employees_from_file(sys.argv[1])
--- Try/js/importEmployees.js
+++ Try/js/importEmployees.js
@@ -1,11 +1,24 @@
 
 function importEmployees() {
 
-    $.ajax
     var fileToLoad = document.getElementById("employees_input").files[0];
+    $.ajax({
+        type: "POST",
+        url: "./import_employees_aux.py",
+        data: { param: fileToLoad},
+        success: callbackFunc
+
+    });
+    function callbackFunc(response) {
+        // do something with the response
+        alert("Great Success!");
+    }
+
+    /*
     var fileReader = new FileReader();
     fileReader.onload = function (fileLoadedEvent) {
         var textFromFileLoaded = fileLoadedEvent.target.result;
     };
     fileReader.readAsText(fileToLoad, "UTF-8");
+    */
 }
