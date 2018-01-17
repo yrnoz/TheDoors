8b42bf5f884ef03e6017f0c0d60b3f8cb9dc3fd7
Koby Brandes <kobybrandes10@gmail.com>
2018-01-17 18:19:25 +0200

    deleting django!

--- mysite/marcador/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-
-urlpatterns = [
-    url(r'^user/(?P<username>[-\w]+)/$', 'marcador.views.doors_user',
-        name='marcador_doors_user'),
-    url(r'^$', 'marcador.views.friends_list', name='marcador_friends_list'),
-]
