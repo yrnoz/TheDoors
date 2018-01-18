0dfdcbe585d0cb99d76918b31c6d265e13de6d80
Yarin <yarin129@gmail.com>
2018-01-18 16:52:51 +0200

    mostly junk

--- project/app/App/Employee.py
+++ project/app/App/Employee.py
@@ -1,4 +1,4 @@
-from app.Database.ManageDB import *
+from App.Database.ManageDB import *
 
 
 class Employee:
--- project/venv/Scripts/activate.bat
+++ project/venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\elyasafb\Desktop\theDoors\project\venv"
+set "VIRTUAL_ENV=D:\AnnualProject\TheDoors\project\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
