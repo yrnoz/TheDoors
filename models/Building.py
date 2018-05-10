0ab2dfa0194a4b0907b8ca4bdd0fd4d2b05e5c6f
elyasaf-boim <elyasafb.95@gmail.com>
2018-05-10 19:44:28 +0300

    #150 fix some tests and some bugs

--- models/Building.py
+++
@@ -1,2 +0,0 @@
-class Building(object):
-    pass
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -25,9 +25,14 @@ def test_user():
     Manager.user_register("user2@yahoo.com", '123', 'username', '123412348', 'eng', 1, 'YAHOO', 'matam')
 
     assert User.get_by_email("email@gmail.com") is None
+    assert User.get_by_id("email@gmail.com") is None
+    assert User.get_by_id("000000000") is not None
     assert User.get_by_email("mang@yahoo.com").company == 'YAHOO'
     assert User.get_by_email('user@yahoo.com').company == 'YAHOO'
     assert User.get_by_email("user2@yahoo.com").company == 'YAHOO'
+
+    assert User.login_valid('email_4@gmail.com', 'username') is True
+    assert User.login_valid('email_4@gmail.com', 'username__') is False
 
     manager = Manager.get_by_email('user@yahoo.com')
     assert manager is None
@@ -36,10 +41,12 @@ def test_user():
     assert manager.delete_user('user@yahoo.com') is True
     assert User.get_by_email('user@yahoo.com') is None
     assert manager.delete_user('email_3@gmail.com') is False
-
-    # user.new_order('15/12/18', ['user2@yahoo.com', 'mang@yahoo.com'], 1, 2)
-    # order_id = 'user@yahoo.com15/12/1812'
-    # user.cancel_order(order_id)
+    assert len(manager.get_friends()) == 0
+    user = User.get_by_email('email_1@gmail.com')
+    assert len(user.get_friends()) == 2
+    assert manager.delete_user('aaaaaaaaaaaaa') is False
+    assert User.min_permission(['email_4@gmail.com', "user2@yahoo.com", "mang@yahoo.com"]) == 1
+    assert User.min_permission(['email_4@gmail.com', 'email_1@gmail.com']) == 3
 
 
 def test_rooms():
