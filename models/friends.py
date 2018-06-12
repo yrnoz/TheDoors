4f74b07b9a352144de048d1800384d50015c8019
Yarin Ozery <yrnoz@users.noreply.github.com>
2018-06-12 18:34:39 +0300

    Update friends.py

--- models/friends.py
+++ models/friends.py
@@ -44,7 +44,7 @@ class Friends(object):
         """
 
         if not Friends.is_friends(user_email, new_friend):
-            _id = friends_key(user_email, to_remove)
+            _id = friends_key(user_email, new_friend)
             Database.insert('friends', {'_id': _id})
             #Friends.save_to_mongodb(user_email, new_friend)
             return True, "success"
