2b978650726b35c4bfbe4cf63bf95177eac4b223
Yarin Ozery <yrnoz@users.noreply.github.com>
2018-06-12 16:51:52 +0300

    an attempt at fixing add friend

--- models/friends.py
+++ models/friends.py
@@ -44,6 +44,7 @@ class Friends(object):
         """
 
         if not Friends.is_friends(user_email, new_friend):
+            Database.insert(collection='friends', data=json(user_email, new_friend))
             #Friends.save_to_mongodb(user_email, new_friend)
             return True, "success"
         return False, "already friends"
